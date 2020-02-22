779359b529f8ec433cc5ea2713499a683e5fb3a2
nuvallo <nuvallo.inc@gmail.com>
2020-02-21 21:51:01 -0500

    add and delete functions work

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -10,6 +11,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setcolorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -18,28 +20,112 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+
+    console.log("Saving edits to color", colorToEdit);
+
+    axiosWithAuth()
+      .put("colors/" + colorToEdit.id, colorToEdit)
+      .then(response => {
+        console.log("Color edited:", response);
+
+        // rebuild color array in same order as before
+        let updatedColors = [];
+
+        for (let i = 0; i < colors.length; i++) {
+          if (colors[i].id === colorToEdit.id) {
+            updatedColors = [...updatedColors, colorToEdit];
+          } else {
+            updatedColors = [...updatedColors, colors[i]];
+          }
+        }
+
+        updateColors(updatedColors);
+      })
+      .catch(error => {
+        console.log("Couldn't edit color:", error);
+      });
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    console.log("Deleting color", color);
+
+    axiosWithAuth()
+      .delete("colors/" + color.id)
+      .then(response => {
+        console.log("Color deleted:", response);
+
+        let remainingColors = colors.filter(
+          existingColor => existingColor.id !== color.id
+        );
+        updateColors(remainingColors);
+      })
+      .catch(error => {
+        console.log("Couldn't delete color:", error);
+      });
+  };
+
+  const addColor = event => {
+    event.preventDefault();
+
+    console.log("Adding color", colorToAdd);
+
+    axiosWithAuth()
+      .post("http://localhost:5000/api/colors", colorToAdd)
+      .then(response => {
+        console.log("Color added:", response);
+
+        updateColors([...colors, colorToAdd]);
+      })
+      .catch(error => {
+        console.log("Couldn't add color:", error);
+      });
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
+
+      <div className="addColorDiv">
+        <form name="addColor">
+          <legend>add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setcolorToAdd({ ...colorToAdd, color: e.target.value })
+              }
+              value={colorToAdd.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setcolorToAdd({
+                  ...colorToAdd,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToAdd.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button onClick={addColor}>add</button>
+          </div>
+        </form>
+      </div>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -81,7 +167,6 @@ const ColorList = ({ colors, updateColors }) => {
         </form>
       )}
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
     </div>
   );
 };
