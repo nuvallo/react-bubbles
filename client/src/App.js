bf08ff25dc55f6f99995544cf5c837e8d885fc6d
nuvallo <nuvallo.inc@gmail.com>
2020-02-21 21:40:24 -0500

    BubblePage being rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -8,11 +10,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <ProtectedRoute exact path="/protected" component={BubblePage} />
+          <Route exact path="/" component={Login} />
+          />
+        </Switch>
       </div>
     </Router>
   );
